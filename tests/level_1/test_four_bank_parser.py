a12f8a082299d8534d136279046c443510444119
DanTheMain <Daniil.Morozov80@yandex.ru>
2023-09-11 13:40:09 +0300

    split multiple test asserts into test functions

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -1,28 +1,59 @@
 import datetime
 import decimal
-from typing import NamedTuple
 from functions.level_1.four_bank_parser import BankCard, SmsMessage, Expense, parse_ineco_expense
 
 
-def test_parse_ineco_expense():
-    test_cards = [BankCard(last_digits='1234', owner='matching_card_owner_1'),
+def test_ineco_expense_return_type():
+    assert isinstance(parse_ineco_expense(SmsMessage(
+        text='12.34 $, "000001234 11.11.11 11:11 test_shop authcode TEST_AUTHCODE',
+        author='test_author',
+        sent_at=datetime.datetime.strptime('11/11/11 11:11:00', '%m/%d/%y %H:%M:%S'),
+    ), [BankCard(last_digits='1234', owner='matching_card_owner_1')]), Expense)
+
+
+def test_ineco_expense_card_choice():
+    expected_last_digits, expected_owner_name = '1234', 'matching_card_owner_1'
+    test_cards = [BankCard(last_digits=f'{expected_last_digits}', owner=f'{expected_owner_name}'),
                   BankCard(last_digits='5432', owner='non_matching_card_owner_1'),
-                  BankCard(last_digits='1234', owner='matching_card_owner_2')]
+                  BankCard(last_digits=f'{expected_last_digits}', owner='non_matching_card_owner_2')]
     test_sms = SmsMessage(
         text='12.34 $, "000001234 11.11.11 11:11 test_shop authcode TEST_AUTHCODE',
         author='test_author',
         sent_at=datetime.datetime.strptime('11/11/11 11:11:00', '%m/%d/%y %H:%M:%S'),
     )
 
-    expected_expense = Expense(
-        amount=decimal.Decimal('12.34'),
-        card=test_cards[0],
-        spent_in='test_shop',
-        spent_at=datetime.datetime.strptime('11/11/11 11:11:00', '%m/%d/%y %H:%M:%S'))
-    assert(expected_expense
-           ==
-           parse_ineco_expense(test_sms, test_cards))
+    actual_test_card = parse_ineco_expense(test_sms, test_cards).card
+    assert(actual_test_card.last_digits == expected_last_digits and actual_test_card.owner == expected_owner_name)
 
 
+def test_ineco_expense_amount():
+    test_amount_str = "12.34"
+    expected_test_amount = decimal.Decimal(f"{test_amount_str}")
 
+    assert (parse_ineco_expense(SmsMessage(
+        text=f'{test_amount_str} $, "000001234 11.11.11 11:11 test_shop authcode TEST_AUTHCODE',
+        author='test_author',
+        sent_at=datetime.datetime.strptime('11/11/11 11:11:00', '%m/%d/%y %H:%M:%S'),
+    ), [BankCard(last_digits='1234', owner='matching_card_owner_1')]).amount == expected_test_amount)
+
+
+def test_ineco_expense_spent_in_name():
+    spent_in_str = 'test_shop'
+
+    assert (parse_ineco_expense(SmsMessage(
+        text=f'12.34 $, "000001234 11.11.11 11:11 {spent_in_str} authcode TEST_AUTHCODE',
+        author='test_author',
+        sent_at=datetime.datetime.strptime('11/11/11 11:11:00', '%m/%d/%y %H:%M:%S'),
+    ), [BankCard(last_digits='1234', owner='matching_card_owner_1')]).spent_in == spent_in_str)
+
+
+def test_ineco_expense_spent_at_datetime():
+    test_dt_str = '11.11.11 11:11'
+    expected_dt = datetime.datetime.strptime('11/11/11 11:11:00', '%m/%d/%y %H:%M:%S')
+
+    assert (parse_ineco_expense(SmsMessage(
+        text=f'12.34 $, "000001234 {test_dt_str} test_shop authcode TEST_AUTHCODE',
+        author='test_author',
+        sent_at=datetime.datetime.strptime('11/11/11 11:11:00', '%m/%d/%y %H:%M:%S'),
+    ), [BankCard(last_digits='1234', owner='matching_card_owner_1')]).spent_at == expected_dt)
 
--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,9 +1,23 @@
 from functions.level_1.one_gender import genderalize
 
 
-def test_genderalize():
+def test_male_genderalization():
+    verb_male = 'verb_male'
+    assert verb_male == genderalize(verb_male, '', 'male')
+
+
+def test_female_genderalization():
     verb_male, verb_female = 'verb_male', 'verb_female'
-    assert (verb_male == genderalize(verb_male, '', 'male'))
-    assert (verb_female == genderalize(verb_male, verb_female, 'female'))
-    assert (verb_female == genderalize('',verb_female, 'no_gender'))
-    assert (verb_female != genderalize(verb_male, '', 'male'))
+    assert verb_female == genderalize(verb_male, verb_female, 'female')
+
+
+def test_default_genderalization():
+    default_gender = 'verb_female'
+    assert default_gender == genderalize('', default_gender, 'no_gender')
+
+
+def test_matching_male_genderalization():
+    verb_male, verb_female, male_gender = 'verb_male', 'verb_female', 'male'
+
+    assert (verb_female != genderalize(verb_male, '', male_gender))
+
